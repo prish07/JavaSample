410adc96146cded89952c26a727a0fabbbaa116c
prish07 <68162385+prish07@users.noreply.github.com>
2025-03-15 10:17:10 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,30 @@
+package JavaSample;
+
+public class Student {
+    private String name;
+    private int age;
+    private double gpa;
+
+    public Student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', age=" + age + ", gpa=" + gpa + "}";
+    }
+}
